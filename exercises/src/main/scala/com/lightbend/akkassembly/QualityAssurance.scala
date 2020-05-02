e04377372c08e85d187ed62dca12d86632d4ec27
André Jerónimo <andre.jeronimo.93@gmail.com>
2020-05-02 15:26:51 +0100

    Exercise 5: Fault Tolerance - completed

--- exercises/src/main/scala/com/lightbend/akkassembly/QualityAssurance.scala
+++ exercises/src/main/scala/com/lightbend/akkassembly/QualityAssurance.scala
@@ -1,16 +1,40 @@
 package com.lightbend.akkassembly
 
 import akka.NotUsed
+import akka.stream.{ActorAttributes, Supervision}
 import akka.stream.scaladsl.Flow
 
 class QualityAssurance {
+
+  /** Decider to use in the inspect flow
+   *  If a car fails the inspect flow must continue, the failed cars is dropped */
+  val inspectDecider: Supervision.Decider = {
+    case _: QualityAssurance.CarFailedInspection => Supervision.Resume
+    case _ => Supervision.Stop
+  }
 
   /** Flow that inspects an unfinished car and produces a car */
   val inspect: Flow[UnfinishedCar, Car, NotUsed] =
     Flow[UnfinishedCar]
     .collect {
-      case unfCar if unfCar.engine.isDefined && unfCar.color.isDefined && unfCar.wheels.size == WheelShop.NumberOfWheelsPerCar =>
-        Car(SerialNumber(), unfCar.color.get, unfCar.engine.get, unfCar.wheels, None)
+      case unfinishedCar if meetsSpecifications(unfinishedCar)=>
+        Car(SerialNumber(), unfinishedCar.color.get, unfinishedCar.engine.get, unfinishedCar.wheels, None)
+      case unfinishedCar =>
+        throw new QualityAssurance.CarFailedInspection(unfinishedCar)
     }
+    .withAttributes(ActorAttributes.supervisionStrategy(inspectDecider))
+
+  /** Checks if an unfinished car meets the specifications of a complete car */
+  def meetsSpecifications(car: UnfinishedCar): Boolean = {
+    if (car.engine.isDefined && car.color.isDefined && car.wheels.size == WheelShop.NumberOfWheelsPerCar) true
+    else false
+  }
+
+}
+
+object QualityAssurance {
+
+  /** Custom exception to be thrown when a the inspection of car fails */
+  class CarFailedInspection(car: UnfinishedCar) extends IllegalStateException(s"Car $car failed inspection")
 
 }
